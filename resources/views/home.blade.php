dbc7f88515f9ba45b25a929d1d1e4df73a5764d8
Haziq <muhdhaziqjobli@gmail.com>
2022-04-10 17:00:09 +0800

    Front Page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,86 @@
-@extends('layouts.app')
+@extends('layouts.front')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<!-- ======= Request Section ======= -->
+<section id="request" class="team section-bg">
+<div class="container" data-aos="fade-up">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <div class="section-title">
+    <h2>Donation Requests</h2>
+    </div>
 
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+
+    <div class="col-4 mt-4">
+        <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
+        <div class="pic"><img src="assets/img/team/team-1.jpg" class="img-fluid" alt=""></div>
+        <div class="member-info">
+            <h4>Walter White</h4>
+            <span>Chief Executive Officer</span>
+            <p>Explicabo voluptatem mollitia et repellat qui dolorum quasi</p>
+            <div class="social">
+            <a href=""><i class="ri-twitter-fill"></i></a>
+            <a href=""><i class="ri-facebook-fill"></i></a>
+            <a href=""><i class="ri-instagram-fill"></i></a>
+            <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
             </div>
         </div>
+        </div>
     </div>
+
+    <div class="col-4 mt-4">
+        <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="200">
+        <div class="pic"><img src="assets/img/team/team-2.jpg" class="img-fluid" alt=""></div>
+        <div class="member-info">
+            <h4>Sarah Jhonson</h4>
+            <span>Product Manager</span>
+            <p>Aut maiores voluptates amet et quis praesentium qui senda para</p>
+            <div class="social">
+            <a href=""><i class="ri-twitter-fill"></i></a>
+            <a href=""><i class="ri-facebook-fill"></i></a>
+            <a href=""><i class="ri-instagram-fill"></i></a>
+            <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+            </div>
+        </div>
+        </div>
+    </div>
+
+    <div class="col-4 mt-4">
+        <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="300">
+        <div class="pic"><img src="assets/img/team/team-3.jpg" class="img-fluid" alt=""></div>
+        <div class="member-info">
+            <h4>William Anderson</h4>
+            <span>CTO</span>
+            <p>Quisquam facilis cum velit laborum corrupti fuga rerum quia</p>
+            <div class="social">
+            <a href=""><i class="ri-twitter-fill"></i></a>
+            <a href=""><i class="ri-facebook-fill"></i></a>
+            <a href=""><i class="ri-instagram-fill"></i></a>
+            <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+            </div>
+        </div>
+        </div>
+    </div>
+
+    <div class="col-4 mt-4">
+        <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="400">
+        <div class="pic"><img src="assets/img/team/team-4.jpg" class="img-fluid" alt=""></div>
+        <div class="member-info">
+            <h4>Amanda Jepson</h4>
+            <span>Accountant</span>
+            <p>Dolorum tempora officiis odit laborum officiis et et accusamus</p>
+            <div class="social">
+            <a href=""><i class="ri-twitter-fill"></i></a>
+            <a href=""><i class="ri-facebook-fill"></i></a>
+            <a href=""><i class="ri-instagram-fill"></i></a>
+            <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+            </div>
+        </div>
+        </div>
+    </div>
+
+    </div>
+
 </div>
+</section><!-- End Request Section -->
 @endsection
